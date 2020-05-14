707f068bc515ed96a50605ac82fde600e55fa67c
hezhe <zhe.he@jyblife.com>
2020-05-14 14:12:33 +0800

    heap sort

--- src/tree/heap/BinaryHeap.java
+++ src/tree/heap/BinaryHeap.java
@@ -67,6 +67,28 @@ public class BinaryHeap {
         for (int i = (array.length - 2) / 2; i >= 0; i--) {
             downAndJust(array, i, array.length);
         }
+        System.out.println("构建小顶堆之后数组是");
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    /**
+     * 堆排序
+     * 如果要从小到大的排序，则构建大顶堆
+     * 如果要从大到小的排序，则构建小顶堆
+     * 因为排序的操作是拿最后一个子节点和头结点交换，然后在调整堆
+     * 拿小顶堆来说，头元素肯定是最小的，最后一个元素与其交换后，开始调整
+     * 这里注意一个点，最后一个元素和头元素交换后，由于小顶堆的特性，子节点一定比父节点大，所以
+     */
+    public void heapSort(int[] array) {
+        //先构建最小堆
+        buildHeap(array);
+        for (int i = array.length - 1; i > 0; i--) {
+            int temp = array[i];
+            array[i] = array[0];
+            array[0] = temp;
+            downAndJust(array, 0, i);
+        }
     }
 
 
@@ -78,6 +100,13 @@ public class BinaryHeap {
 
         array = new int[]{7, 1, 3, 10, 5, 2, 8, 9, 6};
         binaryHeap.buildHeap(array);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[]{1, 3, 2, 6, 5, 7, 8, 9, 10, 0};
+        System.out.println("堆排序之前的数组");
+        System.out.println(Arrays.toString(array));
+        binaryHeap.heapSort(array);
+        System.out.println("堆排序之后的数组");
         System.out.println(Arrays.toString(array));
     }
 
